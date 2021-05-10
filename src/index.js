79b8c8d00848545abde8550d655608a720c5ba58
Andrew Duberstein <andrew.duberstein@instacart.com>
2021-05-09 17:07:06 -0700

    Update fit

--- src/index.js
+++ src/index.js
@@ -11,41 +11,87 @@ import {bisector} from 'd3-array';
 
 import pop from './county-populations.json';
 import shape from './counties.json';
+import './App.css';
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
+const TARGET_POP = 8336817;
+const DEFAULT_METRICS = {numPop: 0, numCounties: 0};
+const CONTINENTAL_US_BBOX = [
+   [-66.885444, 49.384358],
+   [-124.848974, 24.396308],
+];
+const INITIAL_VIEWPORT = {
+    "bearing": 0,
+    "pitch": 0,
+    "latitude": 36.38,
+    "longitude": -96.37,
+    "zoom": 1,
+};
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function App() {
-  const [target, setTarget] = useState("none");
-  const [neighbors, setNeighbors] = useState([]);
-  const [metrics, setMetrics] = useState({});
+  const [target, setTarget] = useState("");
+  const [neighbors, setNeighbors] = useState(null);
+  const [metrics, setMetrics] = useState(DEFAULT_METRICS);
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+  const [computing, setComputing] = useState(false);
+
+  function handleResize({width, height}) {
+    const vw = new WebMercatorViewport({
+      ...viewport, height, width
+    });
+    const { zoom } = vw.fitBounds(CONTINENTAL_US_BBOX);
+    setViewport({...viewport, zoom, height, width});
+  }
 
   function handleHover(info, event) {
-    if (info && info.object) { const targetGeoId = info.object.properties.id;
-      const {geos = [], numPop, numCounties} = checkIntersect(info.object);
+    if (info && info.object) {
+      const targetGeoId = info.object.properties.id;
+      setComputing(true);
+      const {geos = [], numPop = 0, numCounties} = checkIntersect(info.object);
+      setComputing(false);
       setTarget(targetGeoId);
       setNeighbors(geos);
       setMetrics({numPop, numCounties});
     } else {
-      setTarget(0);
-      setNeighbors([]);
-      setMetrics({});
+      setTarget("");
+      setNeighbors(null);
+      setMetrics(DEFAULT_METRICS);
     }
   }
 
+  let msg;
+  if (computing) {
+    msg = (<p>Computing</p>);
+  }
+  else if (target === "06037") {
+    msg = (<p>{metrics.numCounties ? `LA County is 10,081,570 and exceeds the population of NYC` : <em>Mouseover or tap map. Pinch or scroll to zoom.</em>}</p>)
+  } else if (target) {
+    msg = (<p>{`Highlighted area representing ${numberWithCommas(metrics.numPop)} people in ${metrics.numCounties} US counties, compared to 8,336,817 in NYC.`}</p>)
+  }
+  else {
+    msg = (<p><i>Mouseover/tap map, scroll/pinch to zoom</i></p>)
+  }
+
   return (
-    <div style={{fontFamily: 'sans-serif'}}>
-      <div style={{height: '15vh', margin: '10px'}}>
+    <div>
+      <div className='header'>
         <h1>Distributed New York City</h1>
-        <p>{metrics.numCounties ? `Purple area representing ${numberWithCommas(metrics.numPop)} people in ${metrics.numCounties} US counties` : <em>Mouseover map</em>}</p>
+        {msg}
       </div>
-      <div className="deck-container" style={{position: 'relative', top: '10px'}}>
+      <div className="deck-container" >
         <Map
           data={shape}
-          viewState={VIEWPORT}
+          viewState={viewport}
           target={target}
           onHover={handleHover}
+          onResize={handleResize}
           neighbors={neighbors} />
       </div>
     </div>
@@ -54,16 +100,15 @@ function App() {
 
 function checkIntersect(poly) {
   const idsSortedbyDistance = _checkIntersect(poly, shape.features);
-  const TARGET_POP = 8336817;
   let targetSum = 0;
-  const geos = [];
+  const geos = new Set();
   for (const geo of idsSortedbyDistance) {
     const {geoId, summable} = geo;
     targetSum = Number.isFinite(summable) ? targetSum + summable : targetSum;
-    if (targetSum > TARGET_POP) {
-      return {geos, numPop: targetSum - summable, numCounties: geos.length}
+    geos.add(geoId);
+    if (targetSum >= TARGET_POP) {
+      return {geos, numPop: targetSum, numCounties: geos.size}
     }
-    geos.push(geoId);
   }
 }
 
@@ -93,15 +138,7 @@ function _checkIntersect(poly, polyList) {
   return distanceList;
 }
 
-
-const VIEWPORT = {
-    ...(new WebMercatorViewport()).fitBounds([[-124.5, 48.22], [-67.1, 25.31]]),
-    "bearing": 0,
-    "pitch": 0,
-    "zoom": window.screen.clientWidth > 700 ? 3.81 : 2.5
-};
-
-function Map({data, viewState, target, neighbors, onHover}) {
+function Map({data, viewState, target, neighbors, onHover, onResize}) {
   const layer = new GeoJsonLayer({
     id: 'geojson-layer',
     data,
@@ -110,53 +147,34 @@ function Map({data, viewState, target, neighbors, onHover}) {
     stroked: true,
     lineWidthMinPixels: 1,
     updateTriggers: {
-        getFillColor: [target, neighbors],
-        getLineColor: [target, neighbors]
+        getFillColor: [neighbors]
     },
     getFillColor: d => {
-      if (d.properties.id === target) {
-        return [128, 0, 128]
+      if (neighbors && neighbors.has(d.properties.id)) {
+        return [218, 127, 143]
       }
-      if (neighbors && neighbors.includes(d.properties.id)) {
-        return [128, 0, 128]
-      }
-      return [255, 255, 0, 128]
+      return [225, 229, 234, 128]
     },
-    getLineColor: d => {
-      if (d.properties.id === target) {
-        return [255, 255, 255]
-      }
-      if (neighbors && neighbors.includes(d.properties.id)) {
-        return [255, 255, 255]
-      }
-      return [25, 25, 25]
-    },
-    onClick: (info, event) => console.log(info),
-    onHover
+    highlightColor: [218, 127, 143, 128],
+    getLineColor: [25, 25, 25, 128],
+    onHover,
+    autoHighlight: true
   });
 
 
   const deckglRef = useRef(null);
 
-  function onViewportChange({viewState, interactionState, oldViewState}) {
-    console.log('we are changing the viewport')
-    return {
-      height: deckglRef.current.clientHeight,
-      width: deckglRef.current.clientWidth,
-      ...viewState
-    }
-  }
-
   return (
-    <DeckGL ref={deckglRef} initialViewState={VIEWPORT}
+    <DeckGL ref={deckglRef} initialViewState={viewState}
       controller={true}
       layers={[layer]}
-      onViewStateChange={v => console.log(v.viewState)}
-      height={'100vh'}
-      width={'100vw'}
+      useDevicePixels={true}
+      height={'100%'}
+      onResize={onResize} 
+      width={'100%'}
     >
       <View id="map" width="100%">
-        <StaticMap mapStyle={null} />
+        <StaticMap reuseMaps={true} mapStyle={null} />
       </View>
     </DeckGL>);
 }
@@ -164,7 +182,7 @@ function Map({data, viewState, target, neighbors, onHover}) {
 
 ReactDOM.render(
   (
-    <App />
+    <App/>
   ),
   document.getElementById('react-container')
 );
